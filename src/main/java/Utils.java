947cbc133388b7ef255173ada8aa15b8bca5a74b
Кирилл Шишков <shishkov.kirill99@gmail.com>
2025-08-29 23:12:15 +0300

    delete extra date formatter methods

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,93 +1,88 @@
+import Entity.CallDataRecord;
+import Entity.CallType;
+import Entity.CdrDto;
+import Entity.TariffType;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Utils {
+    private static final DateTimeFormatter DATE_PARSE_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+    private static final DateTimeFormatter DATE_REPORT_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public static List<CallDataRecord> parseData(String file) throws IOException {
+    public static CdrDto parseFile(String file) {
         List<CallDataRecord> data = new ArrayList<>();
-        var reader = new BufferedReader(new FileReader(file));
-        while(reader.ready()) {
-            String[] parts = reader.readLine().split(", ");
-            var callType = CallType.fromCode(parts[0]);
-            var phoneNumber = parts[1];
-            var startTime = covertStringToDate(parts[2]);
-            var stopTime = covertStringToDate(parts[3]);
-            var tariffType = TariffType.fromCode(parts[4]);
-            data.add(new CallDataRecord(callType, phoneNumber, startTime, stopTime, tariffType));
+        Set<String> uniqueNumbers = new HashSet<>();
+
+        try (var reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",\\s*");
+                var callType = CallType.fromCode(parts[0]);
+                var phoneNumber = parts[1];
+                var startTime = LocalDateTime.parse(parts[2], DATE_PARSE_FORMATTER);
+                var stopTime = LocalDateTime.parse(parts[3], DATE_PARSE_FORMATTER);
+                var tariffType = TariffType.fromCode(parts[4]);
+                data.add(new CallDataRecord(callType, phoneNumber, startTime, stopTime, tariffType));
+                uniqueNumbers.add(phoneNumber);
+            }
+        } catch (IOException e) {
+            //todo обработать исключение
         }
-        return data;
-    }
-
-    private static LocalDateTime covertStringToDate(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-        LocalDate localDate = LocalDate.parse(date, formatter);
-        LocalTime localTime = LocalTime.parse(date, formatter);
-        return LocalDateTime.of(localDate, localTime);
-    }
-
-    private static String formatDate(LocalDateTime dateTime) {
-        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        return dateTime.format(outputFormatter);
-    }
-
-    public static Set<String> getUniqPhoneNumbers(List<CallDataRecord> data) {
-        Set<String> set = new HashSet<>();
-        for (CallDataRecord callDataRecord: data) {
-            set.add(callDataRecord.getPhoneNumber());
-        }
-        return set;
+        return new CdrDto(data, uniqueNumbers);
     }
 
     public static void generateReport(Path reportsDir, List<CallDataRecord> callDataRecords) {
-        var phoneNumber = callDataRecords.get(0).getPhoneNumber();
-        try(Writer printWriter = new FileWriter(reportsDir + "/" + phoneNumber + ".txt", StandardCharsets.UTF_8, true);
-            BufferedWriter writer = new BufferedWriter(printWriter)
+        var phoneNumber = callDataRecords.get(0).phoneNumber();
+        try (Writer printWriter = new FileWriter(reportsDir + "/" + phoneNumber + ".txt", StandardCharsets.UTF_8, true);
+             BufferedWriter writer = new BufferedWriter(printWriter)
         ) {
             double totalPrice = 0;    // Общая цена всех звонков
             double globalTime = 0;    // Общее время звонков за тарифный период в секундах
             writeReportHeader(writer, callDataRecords.get(0));
 
-            for (CallDataRecord callDataRecord: callDataRecords) {
+            for (CallDataRecord callDataRecord : callDataRecords) {
                 double price = 0;
-                long duration = Duration.between(callDataRecord.getStartTime(), callDataRecord.getStopTime()).toSeconds();
-                switch (callDataRecord.getTariffType()) {
+                long duration = Duration.between(callDataRecord.startTime(), callDataRecord.stopTime()).toSeconds();
+                switch (callDataRecord.tariffType()) {
                     case PER_MINUTE:
-                        price = (double) duration/60 * 1.5;
+                        price = (double) duration / 60 * 1.5;
                         totalPrice += price;
                         break;
                     case COMMON:
-                        if (callDataRecord.getCallType() == CallType.INBOX) {
+                        if (callDataRecord.callType() == CallType.INBOX) {
                             globalTime -= duration;
                         } else {
-                            if (globalTime /60 <= 100) {
-                                price = (double) duration/60 * 0.5;
+                            if (globalTime / 60 <= 100) {
+                                price = (double) duration / 60 * 0.5;
                             } else {
-                                price = (double) duration/60 * 1;
+                                price = (double) duration / 60 * 1;
                             }
                             totalPrice += price;
                         }
                         break;
                     case UNLIMITED:
                         // Если потрачено менее 300 минут
-                        if (globalTime /60 <= 300) {
+                        if (globalTime / 60 <= 300) {
                             totalPrice = 100;
                         }
 
                         // Если звонок происходит после израсходования 300 минут
-                        if (globalTime /60 > 300) {
-                            price = (double) duration/60;
+                        if (globalTime / 60 > 300) {
+                            price = (double) duration / 60;
                             totalPrice += price;
                         }
 
                         // Если во время данного звонка был преодолён порог в 300 минут
-                        if ((globalTime - duration)/60 <= 300 && globalTime /60 > 300) {
+                        if ((globalTime - duration) / 60 <= 300 && globalTime / 60 > 300) {
                             price = (globalTime / 60) - 300;
                             totalPrice = (double) 100 + price;
                         }
@@ -103,9 +98,9 @@ public class Utils {
     }
 
     private static void writeReportHeader(BufferedWriter writer, CallDataRecord callDataRecord) throws IOException {
-        writer.write("Tariff index: " + callDataRecord.getTariffType().code + "\n");
+        writer.write("Tariff index: " + callDataRecord.tariffType().code + "\n");
         writer.write("----------------------------------------------------------------------------\n");
-        writer.write("Report for phone number " + callDataRecord.getPhoneNumber() + ":\n");
+        writer.write("Report for phone number " + callDataRecord.phoneNumber() + ":\n");
         writer.write("----------------------------------------------------------------------------\n");
         writer.write("| Call Type |     Start Time      |     End Time        | Duration | Cost  |\n");
         writer.write("----------------------------------------------------------------------------\n");
@@ -113,7 +108,7 @@ public class Utils {
 
     private static void writeLine(BufferedWriter writer, CallDataRecord callDataRecord, long duration, double price) throws IOException {
         var formattedDuration = String.format("%02d:%02d:%02d", duration / 3600, duration / 60 % 60, duration % 60);
-        writer.write("|     " + callDataRecord.getCallType().code + "    | " + formatDate(callDataRecord.getStartTime()) + " | " + formatDate(callDataRecord.getStopTime()) + " | " + formattedDuration + " |  " + String.format("%(.2f", price).replace(",", ".") + " |\n");
+        writer.write("|     " + callDataRecord.callType().code + "    | " + callDataRecord.startTime().format(DATE_REPORT_FORMATTER) + " | " + callDataRecord.stopTime().format(DATE_REPORT_FORMATTER) + " | " + formattedDuration + " |  " + String.format("%(.2f", price).replace(",", ".") + " |\n");
         writer.write("----------------------------------------------------------------------------\n");
     }
 
